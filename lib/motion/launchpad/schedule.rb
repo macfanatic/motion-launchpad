49ba758fae93103b9580a1ba4768c1df9a9b102f
Matt Brewer <matt.brewer@me.com>
2014-03-24 11:02:11 -0400

    Schedule keeps track of launch count

--- lib/motion/launchpad/schedule.rb
+++ lib/motion/launchpad/schedule.rb
@@ -6,10 +6,22 @@ module Motion
       def initialize(options={}, &block)
         self.preferences_key = options.fetch(:preferences_key, :launch_count)
         @events = []
+
+        handle_launch
       end
 
       def configure
         yield(self) if block_given?
+      end
+
+      private
+
+      def handle_launch
+        if NSUserDefaults.standardUserDefaults[preferences_key].nil?
+          NSUserDefaults.standardUserDefaults[preferences_key] = 1
+        else
+          NSUserDefaults.standardUserDefaults[preferences_key] = NSUserDefaults.standardUserDefaults[preferences_key] + 1
+        end
       end
     end
   end
--- spec/schedule_spec.rb
+++ spec/schedule_spec.rb
@@ -1,10 +1,19 @@
 describe Motion::Launchpad::Schedule do
 
+  before do
+    NSUserDefaults.standardUserDefaults[:launch_count] = nil
+  end
+
   describe '#configure' do
     it "runs the block" do
       instance = Motion::Launchpad::Schedule.new
       instance.configure { true }.should.be.true
     end
+  end
+
+  it "initializes NSUserDefaults store on #new" do
+    instance = Motion::Launchpad::Schedule.new
+    NSUserDefaults.standardUserDefaults[:launch_count].should.be.equal 1
   end
 
 end
